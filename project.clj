2befd1b9a0b3ac776e68d487c90480519c02f010
JLK <taprisiot@rocketmail.com>
2012-05-23 15:50:55 +1000

    update project details

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
-(defproject math "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache/math "2.2"]])
+(defproject jlk/math "0.1-SNAPSHOT"
+  :description "wrappers around apache commons math"
+  :url "https://github.com/taprisiot/jlk.repl"
+  :license {:name "BSD"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.apache/math "3.0"]
+                 [jlk/utility "0.1-SNAPSHOT"]]
+  :plugins [[lein-swank "1.4.0"]])
