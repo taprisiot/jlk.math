865b82a2550161d393b5f31a0a7a7e0754ad007f
JLK <taprisiot@rocketmail.com>
2012-05-26 11:27:19 +1000

    implement a symbol resolving mechanism
    still in development...

--- src/jlk/math/optimization.clj
+++ src/jlk/math/optimization.clj
@@ -1,6 +1,7 @@
 (ns jlk.math.optimization
   (:use [jlk.utility.core :only [exception]]
-        [clojure.set :only (difference)])
+        [clojure.set :only (difference)]
+        [clojure.walk :only [prewalk-replace walk]])
   (:import [org.apache.commons.math3.analysis UnivariateFunction MultivariateFunction]
            [org.apache.commons.math3.analysis.solvers BisectionSolver BracketingNthOrderBrentSolver BrentSolver IllinoisSolver LaguerreSolver MullerSolver MullerSolver2 NewtonSolver PegasusSolver RegulaFalsiSolver RiddersSolver SecantSolver]
            [org.apache.commons.math3.optimization GoalType]
@@ -110,3 +111,49 @@ arguments so-keyed, and the final argument."
                (seq (difference (set arg-keys) (keys m)))]
            (assert (and (seq free?) (empty? err)))
            (fn [x] (apply f (map (assoc m free x) arg-keys))))))))
+
+(defn map-keywords-to-syms
+  "take a map with keyword or string keys and turn into symbol keys"
+  [m]
+  (reduce (fn [m [k v] ] (assoc m (symbol (name k)) v)) {} m))
+
+(defmulti -resolve-expr
+  "take an unresolved count, an expression and a resolved symbol table and return a function of airity the number of unresolved variables with all other symbols replaced with the values in rvals"
+  (fn [x & args] x))
+(defmethod -resolve-expr 0
+  [_ _ _]
+  (exception "expression is fully defined, nothing to solve for"))
+(defmethod -resolve-expr 1
+  [_ expr rvals]
+  (let [lut (reduce (fn [m [k v]] (assoc m k (if v v 'x))) {} rvals)
+        rexpr (prewalk-replace lut expr)]
+    (fn [x] (eval (prewalk-replace {'x x} rexpr)))))
+(defmethod -resolve-expr :default
+  [_ expr rvals]
+  (exception "multiple unresolved variables is not yet implemented"))
+
+;;
+;; do a special case for 1 arg -> solve
+;; the general case -> optimize and can be provided with a vector
+;;
+(defn resolve-expr
+  [expr values]
+  (let [values (map-keywords-to-syms values)
+        ;; get a list of all the symbols in the expression and resolve them if possible
+        exprvals (reduce (fn [m var] (assoc m var (ns-resolve *ns* var)))
+                         {}
+                         (filter symbol? (distinct (flatten (tree-seq seq? identity expr)))))
+        ;; map the resolved values against symbol names
+        rvals (merge exprvals values)
+        ;; work out how many symbols are still unresolved
+        unresolved-count (count (filter nil? (vals rvals)))]
+    (println rvals)
+    (println unresolved-count)
+    (-resolve-expr unresolved-count expr rvals)))
+
+;; examples:
+;; (solve (resolve-expr '(- (* x x) 1) {}) :start 0 :min -10 :max 10)
+;; (solve (resolve-expr '(- (* x x) y) {:y 2}) :start 0 :min -10 :max 10)
+;; --> -1.141
+;; (solve (resolve-expr '(- (* x x) y) {:y 2}) :start 0 :min -1 :max 10)
+;; --> 1.131
