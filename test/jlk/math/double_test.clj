694a9fbc672d5164e3a26b409393ba0ff5481abd
JLK <taprisiot@rocketmail.com>
2012-06-07 17:27:49 +1000

    rename

--- test/jlk/math/double_test.clj
+++
@@ -1,8 +0,0 @@
-(ns jlk.math.double-test
-  (:use clojure.test)
-  (:require [jlk.math.double :as d]))
-
-(deftest atan2-flipped
-  ;; arg order of atan2 is weird, check it
-  (is (== 0 (d/atan2 #_x 1 #_y 0)))
-  (is (< (d/abs (- (d/atan2 0 1) (/ d/π 2))) 0.000001)))
